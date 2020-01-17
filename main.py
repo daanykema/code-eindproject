6dda4067cc7fcc9289a6a0af0ae24af05035da17
daanutrecht <47120632+daanykema@users.noreply.github.com>
2020-01-17 17:18:07 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-import csv
-import math
+import csv, math, os
 
+filepath = 'test/'
 #helperfunction to check if item is float
 def is_float(item):
     return type(item) == float
+
+def clean_sentence(sentence):
+    sentence = sentence.replace('.', '')
+    sentence = sentence.replace(',', '')
+    sentence = sentence.replace('(', '')
+    sentence = sentence.replace(')', '')
+    sentence = sentence.replace('-', '')
+    sentence = sentence.replace('\'', '')
+    sentence = sentence.replace(':', '')
+    sentence = sentence.replace(';', '')
+    sentence = sentence.replace('\"', '')
+    sentence = sentence.replace('\\', '')
+    sentence = sentence.replace('\\', '')
+    sentence = sentence.lower()
+    return sentence
 
 #function to parse csv file and return nested list
 def read_matrix():
@@ -43,7 +58,7 @@ def run_query(matrix, document_vectors):
     query = ['appel','deeg']
     query_vector_len = math.sqrt(len(query))
     firstline = 1
-    similarity_scores = list()
+    similarity_scores = dict()
     for idx, document in enumerate(matrix[0]):
         if firstline:
             firstline = 0
@@ -53,8 +68,45 @@ def run_query(matrix, document_vectors):
             if row[0] in query:
                 if is_float(row[idx]):
                     term_weights += row[idx]
-        similarity_scores.append(term_weights / (query_vector_len * document_vectors[document]))
-    return similarity_scores
+        similarity_scores[document] = term_weights / (query_vector_len * document_vectors[document])
+    return sorted(similarity_scores.items(), key= lambda score:score[1], reverse=True)
 
-doc_vectors = calc_doc_vector(read_matrix())
-print(run_query(read_matrix(), doc_vectors))
+def create_tf_dict():
+    directory = '/users/Daan/Desktop/search_engine/test/'
+    tf_dict = dict()
+    for filename in os.listdir(directory):
+        filewithpath = filepath + filename
+        if filewithpath.endswith(".txt"):
+            f = open(filewithpath)
+            lines = f.read()
+            wordlist = clean_sentence(lines).split()
+            tf_dict2 = dict()
+            for word in wordlist:
+                if word in tf_dict2:
+                    tf_dict2[word] += 1
+                else: 
+                    tf_dict2[word] = 1
+        tf_dict[filename] = tf_dict2
+    return tf_dict
+
+def create_tf_matrix(terms):
+    tf_matrix = list()
+    first_row = list(terms.keys())
+    first_row.insert(0, '')
+    tf_matrix.append(first_row)
+    term_list = list()
+    for key, value in terms.items():
+        for k,v in value.items():
+            if k in term_list:
+                continue
+            else:
+                term_list.append(k)
+
+def main():
+    doc_vectors = calc_doc_vector(read_matrix())
+    #print(run_query(read_matrix(), doc_vectors))
+    #print(create_tf_dict())
+    create_tf_matrix(create_tf_dict())
+
+if __name__ == "__main__":
+    main()
